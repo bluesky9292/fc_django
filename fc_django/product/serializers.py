52f9aac555c5829c79f5a0350fc303e7c49e7e05
Lee DONGWOOK <bluetuss@gmail.com>
2022-03-27 23:36:34 +0900

    DRF 커밋

---
+++ fc_django/product/serializers.py
@@ -0,0 +1,10 @@
+from dataclasses import field, fields
+from pyexpat import model
+from rest_framework import serializers
+from .models import Product
+
+class ProductSerializers(serializers.ModelSerializer):
+
+    class Meta:
+        model = Product
+        fields = '__all__'
